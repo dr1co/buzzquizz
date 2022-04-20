1366eda43dd7a1c8fda5c31bb5a0bd06f1f1d3c5
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-04-20 16:09:56 -0300

    questions creation files

---
+++ assets/js/questionsCreateScript.js
@@ -0,0 +1,8 @@
+function verifyInputs() {
+    const answersLabel = document.getElementsByName('answer-label');
+    const questionsText = document.getElementsByName('question-text');
+    const questionsBackgroundColor = document.getElementsByName('question-background-color');
+    const imagesUrl = document.getElementsByName('image-url');
+
+
+}
