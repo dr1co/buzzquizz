3dbe20e739089c29d0df1d209bd1db96a10ba8b5
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-04-24 17:35:04 -0300

    apply prettier

--- assets/js/criacao-sucesso-quizz-script.js
+++ assets/js/criacao-sucesso-quizz-script.js
@@ -1,18 +1,22 @@
-const quizzBasicInfos = JSON.parse(localStorage.getItem("quizzObjectRequest"))
+const quizzBasicInfos = JSON.parse(localStorage.getItem("quizzObjectRequest"));
 
 function setBasicInfos() {
-    document.querySelector(".quizz-image").style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.62%, rgba(0, 0, 0, 0.8) 100%), url("${quizzBasicInfos.image}")`;
-    document.querySelector(".quizz-image p").innerHTML = `${quizzBasicInfos.title}`;
+  document.querySelector(
+    ".quizz-image"
+  ).style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.62%, rgba(0, 0, 0, 0.8) 100%), url("${quizzBasicInfos.image}")`;
+  document.querySelector(
+    ".quizz-image p"
+  ).innerHTML = `${quizzBasicInfos.title}`;
 }
 
 function viewQuizz() {
-    document.getElementById("sucesso-do-quizz").style.display = "none";
-    //document.getElementById("visualizar-quizz").style.display = "block"; -- Seria para mostrar a página de visualização do quizz, ainda não sei como passar os parâmetros uma vez que essa página não está definida ainda...
+  document.getElementById("sucesso-do-quizz").style.display = "none";
+  //document.getElementById("visualizar-quizz").style.display = "block"; -- Seria para mostrar a página de visualização do quizz, ainda não sei como passar os parâmetros uma vez que essa página não está definida ainda...
 }
 
 function toHome() {
-    document.getElementById("sucesso-do-quizz").style.display = "none";
-    //document.getElementBById("página-home").style.display = "block"; -- Para mostrar a página home
+  document.getElementById("sucesso-do-quizz").style.display = "none";
+  //document.getElementBById("página-home").style.display = "block"; -- Para mostrar a página home
 }
 
 setBasicInfos();
