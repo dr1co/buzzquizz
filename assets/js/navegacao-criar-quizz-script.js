97dd1a4aae038572daaa5dd758b108b6fd2b1e70
dr1co <dricomatoba@hotmail.com>
2022-04-23 21:57:56 -0300

    implementação JS

--- assets/js/navegacao-criar-quizz-script.js
+++ assets/js/navegacao-criar-quizz-script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+function printNewQuizzButton() {
+    const userQuizzes = document.querySelector(".user-quizzes");
+    const userQuizzesList = userQuizzes.querySelectorAll(".user-quizzes-list > li");
+    console.log(userQuizzesList);
+    if(userQuizzesList.length === 0)
+    {
+        userQuizzes.innerHTML = `<div class="create-quizz flex flex-direction-column justify-content-center align-items-center">
+        <p> Você não criou nenhum <br> quizz ainda :( </p>
+        <button onclick="createQuizz()"> Criar quizz </button>
+    </div>`;
+    }
+    else
+    {
+        userQuizzes.innerHTML = `<div class="user-quizzes-title flex align-items-center">
+        <h3> Seus Quizzes </h3>
+        <ion-icon name="add-circle"></ion-icon>
+    </div>` + userQuizzes.innerHTML;
+    }
+}
+
+printNewQuizzButton();
