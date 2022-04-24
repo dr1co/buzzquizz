197c7fdb88f6ea7dd147f44f9dfbfac808e6c9d6
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-04-24 14:55:32 -0300

    saving the quizz ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,22 @@ async function createQuizzRequest() {
   const quizzObjectCreationRequest = JSON.parse(localStorage.getItem(
     "quizzObjectCreationRequest"
   ));
+
   const { data } = await axios.post(
     "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes",
     quizzObjectCreationRequest
   );
 
-  localStorage.setItem(`quizz:${data.id}`, JSON.stringify(data));
+  const quizzIds = JSON.parse(localStorage.getItem('quizzIds'));
+
+  if (!quizzIds) {
+    const ids = [];
+    ids.push(data.id);
+    localStorage.setItem(`quizzIds`, JSON.stringify(ids));
+  } else {
+    quizzIds.push(data.id);
+    localStorage.setItem(`quizzIds`, JSON.stringify(quizzIds));
+  }
 
   return data;
 }
